1db2496be50822b8924e21b5b84b8fa005e5eb94
ginpg <giseltpv@gmail.com>
2022-11-03 18:21:27 -0400

    Little mod

--- base.sql
+++ base.sql
@@ -15,12 +15,12 @@ size 2M;
 --#region Usuario ABD para la creacion de tablas
 create user ABD identified by ABD;
 
---#region Usuarios que obtienen roles
-create user Administrador1 identified by Administrador1;
-create user Organizador1 identified by Organizador1;
---#endregion
-
 grant CREATE TABLE, CREATE SESSION, CREATE VIEW, CREATE TRIGGER, CREATE ROLE to ABD with ADMIN OPTION;
 alter user ABD quota unlimited on soluciones_moviles_datos;
 alter user ABD quota unlimited on soluciones_moviles_index;
+--#endregion
+
+--#region Usuarios que obtienen roles
+create user Administrador1 identified by Administrador1;
+create user Organizador1 identified by Organizador1;
 --#endregion
